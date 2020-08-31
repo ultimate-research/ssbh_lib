20e3e9c62d823093ba557f774aeb3616b7a5d3d4
SMG <scandanavianmountaingoat@gmail.com>
2020-08-30 21:38:16 -0500

    add anim type

--- src/formats/anim.rs
+++ src/formats/anim.rs
@@ -2,14 +2,51 @@ use crate::SsbhArray;
 use crate::SsbhString;
 use serde::Serialize;
 
-use binread::{
-    io::{Read, Seek, SeekFrom},
-    BinRead, BinResult, ReadOptions,
-};
-// TODO: Have an Ssbh<T> struct with an enum for the actual data
-// TODO: Dump all distinct magics/extensions for SSBH
+use binread::BinRead;
+
+#[derive(Serialize, BinRead, Debug)]
+enum AnimType {
+    #[br(magic = 1u64)]
+    Transform,
+    #[br(magic = 2u64)]
+    Visibility,
+    #[br(magic = 4u64)]
+    Material,
+    #[br(magic = 5u64)]
+    Camera,
+}
+
+#[derive(Serialize, BinRead, Debug)]
+struct AnimTrack {
+    name: SsbhString,
+    flags: u32,
+    frame_count: u32,
+    unk3: u32,
+    // TODO: Use some sort of pointer type.
+    data_offset: u32,
+    data_size: u64,
+}
+
+#[derive(Serialize, BinRead, Debug)]
+struct AnimNode {
+    name: SsbhString,
+    tracks: SsbhArray<AnimTrack>,
+}
+
+#[derive(Serialize, BinRead, Debug)]
+struct AnimGroup {
+    anim_type: AnimType,
+    nodes: SsbhArray<AnimNode>,
+}
+
 #[derive(Serialize, BinRead, Debug)]
 pub struct Anim {
     major_version: u16,
     minor_version: u16,
+    frame_count: f32,
+    unk1: u16,
+    unk2: u16,
+    name: SsbhString,
+    animations: SsbhArray<AnimGroup>,
+    buffer: SsbhArray<u8>,
 }
