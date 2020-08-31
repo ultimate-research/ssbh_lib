9f7d42d573c8b1a8bd6f715ac37b5e46452d50b8
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2020-08-31 14:35:00 -0500

    buffer file reads to improve performance

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 use binread::BinReaderExt;
 use ssbh_lib::Ssbh;
 use std::env;
-use std::fs::File;
+use std::fs;
 use std::path::Path;
 use std::time::Instant;
+
+use binread::io::Cursor;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let input_path = Path::new(&args[1]);
-    let mut file = File::open(input_path).expect("Error opening file.");
+    let mut file = Cursor::new(fs::read(input_path).expect("Error opening file."));
 
     let parse_start_time = Instant::now();
     let ssbh = file.read_le::<Ssbh>().unwrap();
