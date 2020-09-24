0d36da0c80f0f7e764e956b77f2d300ceb052873
SMG <scandanavianmountaingoat@gmail.com>
2020-09-23 23:37:45 -0500

    print usage on invalid argument count

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,19 @@ use std::time::Instant;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Usage:");
+        eprintln!("\tssbh_lib_json <ssbh file>");
+        eprintln!("\tssbh_lib_json <ssbh file> <json output>");
+        return;
+    }
 
     let input_path = Path::new(&args[1]);
+
+    // Modify the input if no output is specified to allow dragging a file onto the executable.
     let output_path = if args.len() == 3 {
         PathBuf::from(&args[2])
     } else {
-        // Modify the input if no output path to allow dragging a file onto the executable.
         PathBuf::from(args[1].to_string() + ".json")
     };
 
