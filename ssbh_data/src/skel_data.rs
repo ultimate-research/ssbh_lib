17d96ddd765bae68b3534968c350855f919ee139
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-06-08 18:41:22 -0500

    add initial skel data types, use glam instead of ndarray

--- ssbh_data/src/skel_data.rs
+++ ssbh_data/src/skel_data.rs
@@ -1,14 +1,31 @@
-use ndarray::{arr2, Array2};
 use ssbh_lib::{formats::skel::Skel, Matrix4x4};
+use glam::Mat4;
 
-fn matrix4x4_to_array2(matrix: &Matrix4x4) -> Array2<f32> {
-    arr2(&[
+// TODO: Include major and minor version?
+pub struct SkelData {
+    pub bones: Vec<SkelBoneEntryData>,
+}
+
+pub struct SkelBoneEntryData {
+    pub name: String,
+    pub transform: [[f32; 4]; 4],
+    pub world_transform: [[f32; 4]; 4],
+    pub parent_index: Option<usize>
+    // TODO: Flags?
+}
+
+fn matrix4x4_to_glam(matrix: &Matrix4x4) -> Mat4 {
+    // Glam uses column-major instead of row-major order.
+    Mat4::from_cols_array_2d(&[
         [matrix.row1.x, matrix.row1.y, matrix.row1.z, matrix.row1.w],
         [matrix.row2.x, matrix.row2.y, matrix.row2.z, matrix.row2.w],
         [matrix.row3.x, matrix.row3.y, matrix.row3.z, matrix.row3.w],
         [matrix.row4.x, matrix.row4.y, matrix.row4.z, matrix.row4.w],
     ])
+    .transpose()
 }
+
+// TODO: Should this work with skel data instead?
 
 /// Calculates the combined single bind transform matrix, which determines the resting position of a single bound mesh object.
 /// Each bone transform is multiplied by its parents transform recursively starting with `parent_bone_name` until a root node is reached.
@@ -16,7 +33,7 @@ fn matrix4x4_to_array2(matrix: &Matrix4x4) -> Array2<f32> {
 pub fn calculate_single_bind_transform(
     skel: &Skel,
     parent_bone_name: &str,
-) -> Option<[(f32, f32, f32, f32); 4]> {
+) -> Option<[[f32; 4]; 4]> {
     // Attempt to find the parent containing the single bind transform.
     let index = skel.bone_entries.elements.iter().position(|b| {
         if let Some(bone_name) = b.name.to_str() {
@@ -27,13 +44,13 @@ pub fn calculate_single_bind_transform(
     })?;
 
     // Accumulate transforms of a bone with its parent recursively.
-    let mut transform = matrix4x4_to_array2(skel.transforms.elements.get(index)?);
+    let mut transform = matrix4x4_to_glam(skel.transforms.elements.get(index)?);
     let mut parent_id = skel.bone_entries.elements[index].parent_index;
     while parent_id != -1 {
         let parent_transform = skel.transforms.elements.get(parent_id as usize)?;
-        let parent_transform = matrix4x4_to_array2(parent_transform);
+        let parent_transform = matrix4x4_to_glam(parent_transform);
 
-        transform = transform.dot(&parent_transform);
+        transform = transform.mul_mat4(&parent_transform);
 
         parent_id = skel
             .bone_entries
@@ -42,30 +59,6 @@ pub fn calculate_single_bind_transform(
             .parent_index;
     }
 
-    Some([
-        (
-            transform[[0, 0]],
-            transform[[0, 1]],
-            transform[[0, 2]],
-            transform[[0, 3]],
-        ),
-        (
-            transform[[1, 0]],
-            transform[[1, 1]],
-            transform[[1, 2]],
-            transform[[1, 3]],
-        ),
-        (
-            transform[[2, 0]],
-            transform[[2, 1]],
-            transform[[2, 2]],
-            transform[[2, 3]],
-        ),
-        (
-            transform[[3, 0]],
-            transform[[3, 1]],
-            transform[[3, 2]],
-            transform[[3, 3]],
-        ),
-    ])
+    // Save the result in row-major order.
+    Some(transform.transpose().to_cols_array_2d())
 }
