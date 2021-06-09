6f27fe317c95e32612d99eb34d7a717288cfcddd
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2021-06-08 23:05:52 -0500

    add tests for single bind transform

--- ssbh_data/src/skel_data.rs
+++ ssbh_data/src/skel_data.rs
@@ -1,63 +1,127 @@
 use glam::Mat4;
-use ssbh_lib::{formats::skel::Skel, Matrix4x4};
 
 // TODO: Include major and minor version?
 pub struct SkelData {
-    pub bones: Vec<SkelBoneEntryData>,
+    pub bones: Vec<BoneData>,
 }
 
-pub struct SkelBoneEntryData {
+pub struct BoneData {
     pub name: String,
     pub transform: [[f32; 4]; 4],
     pub world_transform: [[f32; 4]; 4],
     pub parent_index: Option<usize>, // TODO: Flags?
 }
 
-fn matrix4x4_to_glam(matrix: &Matrix4x4) -> Mat4 {
-    // Glam uses column-major instead of row-major order.
-    Mat4::from_cols_array_2d(&[
-        [matrix.row1.x, matrix.row1.y, matrix.row1.z, matrix.row1.w],
-        [matrix.row2.x, matrix.row2.y, matrix.row2.z, matrix.row2.w],
-        [matrix.row3.x, matrix.row3.y, matrix.row3.z, matrix.row3.w],
-        [matrix.row4.x, matrix.row4.y, matrix.row4.z, matrix.row4.w],
-    ])
-    .transpose()
+fn mat4_from_row2d(elements: &[[f32; 4]; 4]) -> Mat4 {
+    Mat4::from_cols_array_2d(&elements).transpose()
 }
 
-// TODO: Should this work with skel data instead?
+impl SkelData {
+    /// Calculates the combined single bind transform matrix, which determines the resting position of a single bound mesh object.
+    /// Each bone transform is multiplied by its parents transform recursively starting with `parent_bone_name` until a root node is reached.
+    /// Returns the resulting matrix in row major order or `None` if no matrix could be calculated for the given `parent_bone_name`.
+    pub fn calculate_single_bind_transform(&self, parent_bone_name: &str) -> Option<[[f32; 4]; 4]> {
+        // Attempt to find the parent containing the single bind transform.
+        let current_index = self.bones.iter().position(|b| b.name == parent_bone_name)?;
 
-/// Calculates the combined single bind transform matrix, which determines the resting position of a single bound mesh object.
-/// Each bone transform is multiplied by its parents transform recursively starting with `parent_bone_name` until a root node is reached.
-/// Returns the resulting matrix in row major order or `None` if no matrix could be calculated for the given `parent_bone_name`.
-pub fn calculate_single_bind_transform(
-    skel: &Skel,
-    parent_bone_name: &str,
-) -> Option<[[f32; 4]; 4]> {
-    // Attempt to find the parent containing the single bind transform.
-    let index = skel.bone_entries.elements.iter().position(|b| {
-        if let Some(bone_name) = b.name.to_str() {
-            bone_name == parent_bone_name
-        } else {
-            false
+        // Accumulate transforms of a bone with its parent recursively.
+        // TODO: There's probably a cleaner way to write this.
+        let mut transform = mat4_from_row2d(&self.bones.get(current_index)?.transform);
+        let mut parent_index = self.bones[current_index].parent_index;
+        while parent_index.is_some() {
+            let parent_transform =
+                mat4_from_row2d(&self.bones.get(parent_index.unwrap())?.transform);
+
+            transform = transform.mul_mat4(&parent_transform);
+
+            parent_index = self.bones.get(parent_index.unwrap())?.parent_index;
         }
-    })?;
 
-    // Accumulate transforms of a bone with its parent recursively.
-    let mut transform = matrix4x4_to_glam(skel.transforms.elements.get(index)?);
-    let mut parent_id = skel.bone_entries.elements[index].parent_index;
-    while parent_id != -1 {
-        let parent_transform = skel.transforms.elements.get(parent_id as usize)?;
-        let parent_transform = matrix4x4_to_glam(parent_transform);
+        // Save the result in row-major order.
+        Some(transform.transpose().to_cols_array_2d())
+    }
+}
 
-        transform = transform.mul_mat4(&parent_transform);
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-        parent_id = skel
-            .bone_entries
-            .elements
-            .get(parent_id as usize)?
-            .parent_index;
+    #[test]
+    fn single_bind_transform_no_parent() {
+        let data = SkelData {
+            bones: vec![BoneData {
+                name: "root".to_string(),
+                transform: [[0f32; 4]; 4],
+                world_transform: [[0f32; 4]; 4],
+                parent_index: None,
+            }],
+        };
+
+        assert_eq!(None, data.calculate_single_bind_transform("parent"));
     }
 
-    // Save the result in row-major order.
-    Some(transform.transpose().to_cols_array_2d())
+    #[test]
+    fn single_bind_transform_single_parent() {
+        // Use unique values to make sure the matrix is correct.
+        let transform = [
+            [0f32, 1f32, 2f32, 3f32],
+            [4f32, 5f32, 6f32, 7f32],
+            [8f32, 9f32, 10f32, 11f32],
+            [12f32, 13f32, 14f32, 15f32],
+        ];
+        let data = SkelData {
+            bones: vec![BoneData {
+                name: "parent".to_string(),
+                transform,
+                world_transform: [[0f32; 4]; 4],
+                parent_index: None,
+            }],
+        };
+
+        assert_eq!(
+            Some(transform),
+            data.calculate_single_bind_transform("parent")
+        );
+    }
+
+    #[test]
+    fn single_bind_transform_multi_parent_chain() {
+        // Use non symmetric matrices to check the transpose.
+        let data = SkelData {
+            bones: vec![
+                BoneData {
+                    name: "parent".to_string(),
+                    transform: [
+                        [1f32, 0f32, 0f32, 0f32],
+                        [0f32, 2f32, 0f32, 0f32],
+                        [0f32, 0f32, 3f32, 1f32],
+                        [0f32, 0f32, 0f32, 1f32],
+                    ],
+                    world_transform: [[0f32; 4]; 4],
+                    parent_index: Some(1),
+                },
+                BoneData {
+                    name: "grandparent".to_string(),
+                    transform: [
+                        [1f32, 0f32, 0f32, 0f32],
+                        [0f32, 2f32, 0f32, 0f32],
+                        [0f32, 0f32, 3f32, 0f32],
+                        [0f32, 0f32, 0f32, 4f32],
+                    ],
+                    world_transform: [[0f32; 4]; 4],
+                    parent_index: None,
+                },
+            ],
+        };
+
+        assert_eq!(
+            Some([
+                [1f32, 0f32, 0f32, 0f32],
+                [0f32, 4f32, 0f32, 0f32],
+                [0f32, 0f32, 9f32, 4f32],
+                [0f32, 0f32, 0f32, 4f32]
+            ]),
+            data.calculate_single_bind_transform("parent")
+        );
+    }
 }
