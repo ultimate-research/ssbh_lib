76211df81bd9abd638116527d47b4b60f645999d
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2022-07-25 17:32:28 -0500

    fix counts for shdr inputs/outputs

--- ssbh_data/src/shdr_data.rs
+++ ssbh_data/src/shdr_data.rs
@@ -2,7 +2,7 @@ use binrw::io::{Cursor, Seek, SeekFrom};
 use ssbh_lib::formats::shdr::{ShaderType, Shdr};
 use std::convert::{TryFrom, TryInto};
 
-use binrw::{BinRead, BinResult};
+use binrw::{binread, BinRead, BinResult};
 // Smush Shaders:
 // binary data header is always at offset 2896?
 // header for program binary is 80 bytes
@@ -21,15 +21,17 @@ pub struct ShaderEntryData {
     pub unk1: BinaryData,
 }
 
+// TODO: Create a type for string offset + length?
+// TODO: Parse strings using binrw?
 // 108 Bytes
 #[derive(Debug, BinRead)]
 pub struct UnkEntry {
     pub unk1: u32,
-    pub offset: u32,
+    pub name_offset: u32,
     #[br(pad_after = 32)]
-    pub length: u32,
-    pub unk2_used_size_in_bytes: u32,
-    pub unk3: u32,
+    pub name_length: u32,
+    pub used_size_in_bytes: u32, // used size of this uniform buffer?
+    pub unk3: u32,               // number of parameters in the buffer?
     pub unk4: u32,
     pub unk5: i32,
     #[br(pad_after = 44)]
@@ -39,10 +41,10 @@ pub struct UnkEntry {
 // 164 Bytes
 #[derive(Debug, BinRead)]
 pub struct UnkEntry2 {
-    pub offset: u32,
+    pub name_offset: u32,
     #[br(pad_after = 32)]
-    pub length: u32,
-    pub data_type: u32,    // TODO: Data type?
+    pub name_length: u32,
+    pub data_type: UnkEntry2DataType,
     pub entry1_index: i32, // TODO: associated index into the first section entries?
     pub uniform_buffer_offset: i32,
     pub unk4: u32,
@@ -53,12 +55,29 @@ pub struct UnkEntry2 {
     pub unk8: u32,
 }
 
+#[derive(Debug, BinRead)]
+#[binread(repr(u32))]
+pub enum UnkEntry2DataType {
+    Boolean = 0, // 4 bytes
+    Unk4 = 4,
+    Unk7 = 7,
+    Unk20 = 20,
+    Unk36 = 36,
+    Unk37 = 37,
+    Unk38 = 38,
+    Vector4 = 39,   // 16 bytes
+    Matrix4x4 = 50, // ??? bytes
+    Sampler2D = 67,
+    SamplerCube = 69,
+}
+
+// TODO: Is there a standard name for in/out keywords in shading languages?
 // 92 Bytes
 #[derive(Debug, BinRead)]
-pub struct UnkEntry3 {
-    pub offset: u32,
+pub struct AttributeEntry {
+    pub name_offset: u32,
     #[br(pad_after = 32)]
-    pub length: u32,
+    pub name_length: u32,
     pub unk1: u32,
     pub unk2: u32,
     pub unk3: i32,
@@ -66,18 +85,27 @@ pub struct UnkEntry3 {
     pub unk4: u32,
 }
 
+// TODO: Are all relative offsets relative to entry_offset?
 #[derive(Debug, BinRead)]
 pub struct UnkHeader {
     pub file_end_relative_offset: u32,
     #[br(pad_after = 36)]
-    pub string_info_section_offset: u32,
+    pub entry_offset: u32,
 
-    pub count1: u32,
+    pub entry1_count: u32,
     pub unk1: u32,
-    pub count2: u32,
-    pub relative_offset2: u32,
-    #[br(pad_after = 32)]
-    pub count3: u32,
+    pub entry2_count: u32,
+    pub entry2_relative_offset: u32,
+    pub input_count: u32,
+    pub input_relative_offset: u32,
+    pub output_count: u32,
+    pub output_relative_offset: u32,
+
+    pub unk3: u32,
+    pub unk4: u32,
+    pub unk5: u32,
+    pub unk6: u32,
+    pub unk7: u32,
 
     pub string_info_end_relative_offset: u32,
     pub string_section_length: u32,
@@ -94,68 +122,88 @@ impl BinaryData {
         // Some sort of header for the string section?
         reader.seek(SeekFrom::Start(288))?;
         let header: UnkHeader = reader.read_le()?;
+        println!("{:#?}", header);
 
-        let string_info_section_offset2 =
-            header.string_info_section_offset + header.relative_offset2;
-        let string_section_offset =
-            header.string_info_section_offset + header.string_section_relative_offset;
+        let string_section_offset = header.entry_offset + header.string_section_relative_offset;
 
         // TODO: Handle this using BinRead?
-        reader.seek(SeekFrom::Start(header.string_info_section_offset as u64))?;
-        for _ in 0..header.count1 {
+        reader.seek(SeekFrom::Start(header.entry_offset as u64))?;
+        for _ in 0..header.entry1_count {
             let before_struct = reader.stream_position()?;
             let entry: UnkEntry = reader.read_le()?;
-            let current_pos = reader.stream_position()?;
-            // println!("{:?}", current_pos);
+            let after_struct = reader.stream_position()?;
 
             reader.seek(SeekFrom::Start(
-                (string_section_offset + entry.offset) as u64,
+                (string_section_offset + entry.name_offset) as u64,
             ))?;
             let text: NullString = reader.read_le()?;
 
             // TODO: We can use the length to create a custom reader.
             println!("{:?}, {:?}", text.into_string(), before_struct);
             println!("{:#?}", entry);
-            reader.seek(SeekFrom::Start(current_pos))?;
+            reader.seek(SeekFrom::Start(after_struct))?;
         }
-
         println!();
 
-        reader.seek(SeekFrom::Start(string_info_section_offset2 as u64))?;
-        for _ in 0..header.count2 {
+        reader.seek(SeekFrom::Start(
+            (header.entry_offset + header.entry2_relative_offset) as u64,
+        ))?;
+        for _ in 0..header.entry2_count {
             let before_struct = reader.stream_position()?;
             let entry: UnkEntry2 = reader.read_le()?;
-            let current_pos = reader.stream_position()?;
+            let after_struct = reader.stream_position()?;
 
             reader.seek(SeekFrom::Start(
-                (string_section_offset + entry.offset) as u64,
+                (string_section_offset + entry.name_offset) as u64,
             ))?;
             let text: NullString = reader.read_le()?;
 
             // TODO: We can use the length to create a custom reader.
             println!("{:?}, {:?}", text.into_string(), before_struct);
             println!("{:#?}", entry);
-            reader.seek(SeekFrom::Start(current_pos))?;
+            reader.seek(SeekFrom::Start(after_struct))?;
         }
-
         println!();
 
-        // What determines the output count?
-        // HACK: Add 1 for the output.
-        for _ in 0..(header.count3 + 1) {
+        // Inputs
+        reader.seek(SeekFrom::Start(
+            (header.entry_offset + header.input_relative_offset) as u64,
+        ))?;
+        for _ in 0..header.input_count {
             let before_struct = reader.stream_position()?;
-            let entry: UnkEntry3 = reader.read_le()?;
-            let current_pos = reader.stream_position()?;
+            let entry: AttributeEntry = reader.read_le()?;
+            let after_struct = reader.stream_position()?;
 
             reader.seek(SeekFrom::Start(
-                (string_section_offset + entry.offset) as u64,
+                (string_section_offset + entry.name_offset) as u64,
             ))?;
             let text: NullString = reader.read_le()?;
 
             // TODO: We can use the length to create a custom reader.
             println!("{:?}, {:?}", text.into_string(), before_struct);
             println!("{:#?}", entry);
-            reader.seek(SeekFrom::Start(current_pos))?;
+            reader.seek(SeekFrom::Start(after_struct))?;
+        }
+        println!();
+
+        // Outputs
+        reader.seek(SeekFrom::Start(
+            (header.entry_offset + header.output_relative_offset) as u64,
+        ))?;
+        for _ in 0..header.output_count {
+            let before_struct = reader.stream_position()?;
+            let entry: AttributeEntry = reader.read_le()?;
+            let after_struct = reader.stream_position()?;
+
+            reader.seek(SeekFrom::Start(
+                (string_section_offset + entry.name_offset) as u64,
+            ))?;
+            let text: NullString = reader.read_le()?;
+
+            // TODO: We can use the length to create a custom reader.
+            println!("{:?}, {:?}", text.into_string(), before_struct);
+            println!("{:#?}", entry);
+            reader.seek(SeekFrom::Start(after_struct))?;
         }
 
         Ok(BinaryData {})
