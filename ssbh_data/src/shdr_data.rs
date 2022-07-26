43e9d68cea3443b231ee955e863fde4c37582e6d
ScanMountGoat <scandanavianmountaingoat@gmail.com>
2022-07-25 20:04:45 -0500

    add missing shdr data types

--- ssbh_data/src/shdr_data.rs
+++ ssbh_data/src/shdr_data.rs
@@ -21,12 +21,42 @@ pub struct ShaderEntryData {
     pub unk1: BinaryData,
 }
 
+// TODO: Represent the entire binary data using binrw?
+#[derive(Debug)]
+pub struct BinaryData {}
+
+// TODO: Are all relative offsets relative to entry_offset?
+#[derive(Debug, BinRead)]
+pub struct UnkHeader {
+    pub file_end_relative_offset: u32,
+    pub entry_offset: u32,
+    // All zeros?
+    #[br(pad_after = 32)]
+    pub unk_header_1: u32,
+
+    pub entry1_count: u32,
+    pub entry1_relative_offset: u32,
+    pub uniform_count: u32,
+    pub uniform_relative_offset: u32,
+    pub input_count: u32,
+    pub input_relative_offset: u32,
+    pub output_count: u32,
+    pub output_relative_offset: u32,
+    pub unk3: u32,
+    pub unk4: u32,
+    pub unk5: u32,
+    pub unk6: u32,
+    pub unk7: u32,
+    pub string_info_end_relative_offset: u32,
+    pub string_section_length: u32,
+    pub string_section_relative_offset: u32,
+}
+
 // TODO: Create a type for string offset + length?
 // TODO: Parse strings using binrw?
 // 108 Bytes
 #[derive(Debug, BinRead)]
 pub struct UnkEntry {
-    pub unk1: u32,
     pub name_offset: u32,
     #[br(pad_after = 32)]
     pub name_length: u32,
@@ -34,17 +64,17 @@ pub struct UnkEntry {
     pub unk3: u32,               // number of parameters in the buffer?
     pub unk4: u32,
     pub unk5: i32,
-    #[br(pad_after = 44)]
+    #[br(pad_after = 48)]
     pub unk6: i32,
 }
 
 // 164 Bytes
 #[derive(Debug, BinRead)]
-pub struct UnkEntry2 {
+pub struct UniformEntry {
     pub name_offset: u32,
     #[br(pad_after = 32)]
     pub name_length: u32,
-    pub data_type: UnkEntry2DataType,
+    pub data_type: DataType,
     pub entry1_index: i32, // TODO: associated index into the first section entries?
     pub uniform_buffer_offset: i32,
     pub unk4: u32,
@@ -55,65 +85,53 @@ pub struct UnkEntry2 {
     pub unk8: u32,
 }
 
-#[derive(Debug, BinRead)]
-#[binread(repr(u32))]
-pub enum UnkEntry2DataType {
-    Boolean = 0, // 4 bytes
-    Unk4 = 4,
-    Unk7 = 7,
-    Unk20 = 20,
-    Unk36 = 36,
-    Unk37 = 37,
-    Unk38 = 38,
-    Vector4 = 39,   // 16 bytes
-    Matrix4x4 = 50, // ??? bytes
-    Sampler2D = 67,
-    SamplerCube = 69,
-}
-
-// TODO: Is there a standard name for in/out keywords in shading languages?
+// TODO: Is there better name for in/out keywords in shading languages?
 // 92 Bytes
 #[derive(Debug, BinRead)]
 pub struct AttributeEntry {
     pub name_offset: u32,
     #[br(pad_after = 32)]
     pub name_length: u32,
-    pub unk1: u32,
+    pub data_type: DataType,
     pub unk2: u32,
     pub unk3: i32,
     #[br(pad_after = 36)]
     pub unk4: u32,
 }
 
-// TODO: Are all relative offsets relative to entry_offset?
-#[derive(Debug, BinRead)]
-pub struct UnkHeader {
-    pub file_end_relative_offset: u32,
-    #[br(pad_after = 36)]
-    pub entry_offset: u32,
-
-    pub entry1_count: u32,
-    pub unk1: u32,
-    pub entry2_count: u32,
-    pub entry2_relative_offset: u32,
-    pub input_count: u32,
-    pub input_relative_offset: u32,
-    pub output_count: u32,
-    pub output_relative_offset: u32,
-
-    pub unk3: u32,
-    pub unk4: u32,
-    pub unk5: u32,
-    pub unk6: u32,
-    pub unk7: u32,
-
-    pub string_info_end_relative_offset: u32,
-    pub string_section_length: u32,
-    pub string_section_relative_offset: u32,
+// TODO: Types are all aligned/padded?
+#[derive(Debug, BinRead, PartialEq, Eq, Clone, Copy)]
+#[binread(repr(u32))]
+pub enum DataType {
+    Boolean = 0, // 4 bytes
+    /// A single 32-bit signed integer like gl_InstanceID.
+    Int = 4,
+    // TODO: What is this type?
+    Unk7 = 7,
+    /// A single 32-bit unsigned integer.
+    UnsignedInt = 20,
+    /// 3 32-bit unsigned integers like gl_GlobalInvocationID .
+    UVec3 = 22,
+    /// A single 32-bit float.
+    Float = 36,
+    /// 2 32-bit floats.
+    Vector2 = 37,
+    /// 3 32-bit floats.
+    Vector3 = 38,
+    // 4 32-bit floats like gl_Position.
+    Vector4 = 39,
+    Matrix4x4 = 50, // TODO: Is this actually a full matrix?
+    /// sampler2D uniform in GLSL.
+    Sampler2d = 67,
+    /// sampler3D uniform in GLSL.
+    Sampler3d = 68,
+    /// samplerCube uniform in GLSL.
+    SamplerCube = 69,
+    /// sampler2DArray uniform in GLSL.
+    Sampler2dArray = 73,
+    /// image2D uniform in GLSL.
+    Image2d = 103,
 }
-
-#[derive(Debug)]
-pub struct BinaryData {}
 
 impl BinaryData {
     pub fn from_bytes(bytes: &[u8]) -> BinResult<Self> {
@@ -127,7 +145,9 @@ impl BinaryData {
         let string_section_offset = header.entry_offset + header.string_section_relative_offset;
 
         // TODO: Handle this using BinRead?
-        reader.seek(SeekFrom::Start(header.entry_offset as u64))?;
+        reader.seek(SeekFrom::Start(
+            (header.entry_offset + header.entry1_relative_offset) as u64,
+        ))?;
         for _ in 0..header.entry1_count {
             let before_struct = reader.stream_position()?;
             let entry: UnkEntry = reader.read_le()?;
@@ -146,11 +166,11 @@ impl BinaryData {
         println!();
 
         reader.seek(SeekFrom::Start(
-            (header.entry_offset + header.entry2_relative_offset) as u64,
+            (header.entry_offset + header.uniform_relative_offset) as u64,
         ))?;
-        for _ in 0..header.entry2_count {
+        for _ in 0..header.uniform_count {
             let before_struct = reader.stream_position()?;
-            let entry: UnkEntry2 = reader.read_le()?;
+            let entry: UniformEntry = reader.read_le()?;
             let after_struct = reader.stream_position()?;
 
             reader.seek(SeekFrom::Start(
